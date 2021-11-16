f647ab9e7a0f881849e597c7454795083ac2b820
Envil2001 <68606253+Envil2001@users.noreply.github.com>
2021-11-17 00:59:19 +0200

    deploy my react app to github pages

--- .history/src/index_20211114195649.js
+++ .history/src/index_20211114195649.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
@@ -8,11 +8,11 @@ import {store} from './store/index';
 import './FIREBASE';
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
